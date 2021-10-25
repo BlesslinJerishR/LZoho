35510244e05ab8514ebb6fd4325dda1eeb652e53
Mastermindx33 <mastermindx33@gmail.com>
2021-10-25 16:41:59 +0530

    deploy @dupes_alt_0

--- dupes.py
+++ dupes.py
@@ -1,18 +1,28 @@
 def dupes_remover(l1):
     o = ""
     dupe = []
-    for x in l1:
-        if(x in o):
-            dupe.append(x)
-            pass
-        else:
-            o = x + o + '0'
-    print("The Origin : ",o)
+    if len(l1) <= 4:
+        for x in l1:
+            if(x in o):
+                dupe.append(x)
+                pass
+            else:
+                o = x + o + '0'
+        print("The Origin : ",o,end="")
+        print(o)
+    else:
+        print("[!Buggu] --> Innu Kannu pudikala  , therinja enaku sollu kudu .")
 
 n1 = list(input("Enter N1 : "))
+dupes_remover(n1)
 
 # @test1
-print(n1)
+# print(n1)
 
-dupes_remover(n1)
+# Bugs Greater than 4 Digits
+
+# Input 2:  
+# 1 2 2 5 6 9 5 2  
+# Output 2:  
+# 1 2 0 5 6 9 0 2 
             
